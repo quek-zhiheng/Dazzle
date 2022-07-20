0a831fa44e05b9b631e111b56566b145d508a92f
Quek Zhi Heng <quekzzss@gmail.com>
2022-07-20 18:17:54 +0800

    update db initialisation

--- server/server.py
+++ server/server.py
@@ -2,10 +2,17 @@ from flask import Flask
 import psycopg2
 from database.db_init import create_db
 from database.keys import *
-import datetime
 
 app = Flask(__name__)
 
+# initialise db if required
+if DB_RESET != 'False':
+    print('Resetting database...')
+    create_db()
+    print('Database reset.')
+    
+
+# establish connection to db
 def connect_db():
     """
     Connect to the PostgreSQL database.  Returns a database connection.
